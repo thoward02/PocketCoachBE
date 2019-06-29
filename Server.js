d0e6415ef858cfa485c9fc013205cd97ee4905de
thoward02 <36546676+thoward02@users.noreply.github.com>
2019-06-29 11:37:38 -0400

    Added base files

---
+++ Server.js
@@ -0,0 +1,22 @@
+class Server{
+
+  constructor(Settings){
+
+
+
+
+  }
+
+
+
+}
+
+const Settings = {
+  "https"    : "127.0.0.1",
+  "Internal" : "127.0.0.1"
+}
+
+
+const APP = new Server(Settings);
+
+APP.Start();
