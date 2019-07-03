ddd12e8e173e59d95a4ea46b0fb12a95a4fa1319
Thoward <36546676+thoward02@users.noreply.github.com>
2019-07-02 20:43:16 -0400

    Map Pathing written

--- Core/GetFunctions.js
+++ Core/GetFunctions.js
@@ -6,13 +6,36 @@ class GetFunctions{
     this.Functions = {
 
       //For testing
-      "Test" : {
-        "Path" : "/GetTest",
+      "MapGuides" : {
+        "Path" : "/Api/Guides/Maps/:Map",
         "Funct" : function(Request, Response){
+          //Setup var
+          let fs  = require("fs");
+          let Map = Request.params.Map;
+          let MapPool = ["Hanamura","Horizon_Lunar_Colony","Paris","Temple_of_Anubis","Volskaya_Industries","Dorado","Junkertown","Rialto","Route_66","Watchpoint_Gibraltar","Blizzard_World","Eichenwalde","Hollywood","King's_Row","Numbani","Busan","Ilios","Lijiang_Tower","Nepal","Oasis"];
+          let HasMap  = false;
 
-          console.log("Test");
+          for(var items in MapPool){
+            if(MapPool[items]  ==  Map) HasMap = true
+          }
 
-          Response.end("OWO");
+          //If user inputted right map, find map data
+          if(HasMap){
+            let MapData = fs.readFileSync("./Core/AppData/Guides/Maps/MapData/" + Map + ".json");
+
+            fs = null;
+
+            Response.end(MapData);
+          }
+          //If the map input isn't right
+          else{
+            fs = null;
+            Response.end(JSON.stringify({
+              "Success" : false,
+              "Reason"  : "No map found"
+            }));
+          }
+
 
         }
 
